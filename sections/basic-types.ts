14beaf854e490415fa36ea69ed38d31a1e12972b
Saray Monsalvo <saraymonsalvo31@gmail.com>
2026-05-24 22:44:10 -0500

    Nuevos términos y archivos

---
+++ sections/basic-types.ts
@@ -0,0 +1,25 @@
+
+
+export let firstName: string = "Saray";
+let lastName: string = "Monsalvo";
+
+console.log( firstName, lastName );
+
+let fullName: string = firstName + " " + lastName;
+
+console.log( fullName );
+
+let number1: number = 10;
+let number2: number = 20;
+
+console.log( number1 - number2 );
+
+let isActive: boolean = true;
+let isRunning: boolean = false;
+let hasKids: boolean = true;
+
+isActive = !isActive;
+
+console.log( "isActive:", isActive );
+console.log( "isRunning:", isRunning );
+console.log( "hasKids:", hasKids );
